3ea35a0b1754f36a5693461e41011921b97c3fcf
maciejeicam <maciej_michalik@onet.pl>
2022-12-16 15:13:10 +0100

    create convert rules

--- temperatureConverter/CtoFVC.swift
+++ temperatureConverter/CtoFVC.swift
@@ -10,19 +10,41 @@ import UIKit
 class CtoFVC: UIViewController {
     
     var titleLabel = UILabel()
-    let ctoFConvLabel = UILabel()
-    
-    
+    let rulesCF = UILabel()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
         
     }
+    
     private func setupView(){
         view.backgroundColor = .systemBackground
-    }
-    func ctoFConv(){
-        
+        rulesToConvert()
     }
     
+    func rulesToConvert(){
+        view.addSubview(rulesCF)
+        rulesCF.translatesAutoresizingMaskIntoConstraints = false
+        ctoFSubview()
+        
+        NSLayoutConstraint.activate([
+            rulesCF.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            rulesCF.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            rulesCF.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        
+        ])
+        
+    }
+    func ctoFSubview(){
+        rulesCF.text = "To convert Celcius to Farenthide write a number"
+        rulesCF.textColor = .systemBackground
+        rulesCF.font = .systemFont(ofSize: 40, weight: .semibold)
+        rulesCF.textAlignment = .center
+        rulesCF.lineBreakMode = .byWordWrapping
+        rulesCF.numberOfLines = 0
+    }
 }
+
+
+
